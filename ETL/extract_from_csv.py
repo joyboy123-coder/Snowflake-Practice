93605115b43b9b5ae0ec723e95e6b2f8cabec30f
joyboy123-coder <your-email@example.com>
2025-03-16 22:46:45 +0530

    Fixed extract script

--- ETL/extract_from_csv.py
+++ ETL/extract_from_csv.py
@@ -1,3 +1,10 @@
 import pandas as pd  
+
+# Load the dataset again
 df = pd.read_csv(r"C:\Users\yamin\Downloads\messy_orders.csv")  
-df.to_csv(r"C:\Users\yamin\Downloads\extracted_orders.csv", index=False)  
+
+# Save the full dataset to extracted_orders.csv (CHECK THE FILE AFTER THIS)
+df.to_csv(r"C:\Users\yamin\Snowflake-Practice\ETL\extracted_orders.csv", index=False)  
+
+# Print first 10 rows to check if extraction worked
+print(df.head(10))
