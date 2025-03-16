ca15d5971ebf69a44522bcc577bb1320bec5a42b
joyboy123-coder <your-email@example.com>
2025-03-17 00:56:10 +0530

    Added Snowflake load script

--- ETL/load/load_to_snowflake.py
+++ ETL/load/load_to_snowflake.py
@@ -1,1 +1,23 @@
- 
+from snowflake.snowpark import Session 
+
+import pandas as pd 
+
+connection_parameters = {
+    "user": "joyboy",
+    "account": "sqohvej-tt08915",
+    "password": "VamsiDiya12345",
+    "warehouse": "COMPUTE_WH",
+    "database": "PEOPLE_DB",
+    "schema": "PEOPLE_SCHEMA"
+}
+
+
+session = Session.builder.configs(connection_parameters).create()
+
+df = pd.read_csv(r"C:\Users\yamin\Snowflake-Practice\ETL\transform\cleaned_data.csv")
+
+df.columns = df.columns.str.upper()
+
+session.write_pandas(df,'PEOPLE',auto_create_table=True)
+
+print("✅ Snowflake connection successful!")
