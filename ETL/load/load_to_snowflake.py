fe8a84462cd5fd555a6a4dcce875a24c15aa5965
joyboy123-coder <your-email@example.com>
2025-03-18 11:12:11 +0530

    updated snowflake .py file

--- ETL/load/load_to_snowflake.py
+++ ETL/load/load_to_snowflake.py
@@ -1,23 +1,29 @@
-from snowflake.snowpark import Session 
+import os
+from snowflake.snowpark import Session
+import pandas as pd
+from dotenv import load_dotenv
 
-import pandas as pd 
+# Load environment variables
+load_dotenv()
 
+# Snowflake connection parameters
 connection_parameters = {
-    "user": "joyboy",
-    "account": "sqohvej-tt08915",
-    "password": "VamsiDiya12345",
-    "warehouse": "COMPUTE_WH",
-    "database": "PEOPLE_DB",
-    "schema": "PEOPLE_SCHEMA"
+    "user": os.getenv("SNOWFLAKE_USER"),
+    "account": os.getenv("SNOWFLAKE_ACCOUNT"),
+    "password": os.getenv("SNOWFLAKE_PASSWORD"),
+    "warehouse": os.getenv("SNOWFLAKE_WAREHOUSE"),
+    "database": os.getenv("SNOWFLAKE_DATABASE"),
+    "schema": os.getenv("SNOWFLAKE_SCHEMA")
 }
 
-
+# Create Snowflake session
 session = Session.builder.configs(connection_parameters).create()
 
+# Read cleaned data
 df = pd.read_csv(r"C:\Users\yamin\Snowflake-Practice\ETL\transform\cleaned_data.csv")
-
 df.columns = df.columns.str.upper()
 
-session.write_pandas(df,'PEOPLE',auto_create_table=True)
+# Load data into Snowflake
+session.write_pandas(df, 'PEOPLE', auto_create_table=True)
 
 print("✅ Snowflake connection successful!")
