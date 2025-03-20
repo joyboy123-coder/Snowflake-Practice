2926244847a85085071b0e88b942ac82b30b77f4
joyboy123-coder <your-email@example.com>
2025-03-21 00:49:35 +0530

    Added materialized views SQL file

--- Snowflake/materialized_views/materialized_views.sql
+++ Snowflake/materialized_views/materialized_views.sql
@@ -1,1 +1,20 @@
- 
+-- Run this Query and check the query duration 
+
+select year(sale_date) , count(*) from sales
+group by year(sale_date);
+
+
+--  Create a Materialized View (MV) on the Sales table
+
+CREATE MATERIALIZED VIEW sales_mv AS 
+select year(sale_date) , count(*) from sales
+group by year(sale_date)
+
+--  Query the Materialized View (Faster than querying raw data) and check the query duration 
+SELECT * FROM sales_mv;
+
+--  Refresh the Materialized View Manually (If Needed)
+ALTER MATERIALIZED VIEW sales_mv REBUILD;
+
+--  Drop the Materialized View (If No Longer Needed)
+DROP MATERIALIZED VIEW sales_mv;
